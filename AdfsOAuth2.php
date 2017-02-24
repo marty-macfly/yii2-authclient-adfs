bdf47f48e7fc301eef8bae31f816b8af0d3ad6c4
Charles DELFLY <c.delfly@linkbynet.com>
2017-02-24 18:27:18 +0100

    first commit

---
+++ AdfsOAuth2.php
@@ -0,0 +1,59 @@
+<?php
+namespace macfly\authclient;
+
+use Yii;
+use yii\authclient\OAuth2;
+use yii\web\HttpException;
+
+use Emarref\Jwt\Jwt;
+
+class AdfsOAuth2 extends OAuth2
+{
+	public $xoauthDisplayname	= null;
+	public $resource					= null;
+
+  /**
+   * @inheritdoc
+   */
+  protected function initUserAttributes()
+  {
+    $jwt    = new Jwt();
+    $token  = $jwt->deserialize($this->getAccessToken()->getToken());
+    return json_decode($token->getPayload()->jsonSerialize(), true);
+  }
+
+  /**
+   * @inheritdoc
+   */
+  public function buildAuthUrl(array $params = [])
+  {
+    $returnUrl      = $this->getReturnUrl();
+    $defaultParams  = [
+				'resource'						=> substr($returnUrl, 0, strpos($returnUrl, '?')),
+				'xoauth_displayname'	=> $this->getXoauthDisplayname(),
+			];
+
+    return parent::buildAuthUrl(array_merge($defaultParams, $params));
+  }
+
+	protected function getXoauthDisplayname()
+  {
+		return is_null($this->xoauthDisplayname) ? Yii::$app->name : $this->xoauthDisplayname;
+  }
+
+  /**
+   * @inheritdoc
+   */
+  protected function defaultName()
+  {
+		return 'adfsoauth2';
+  }
+
+  /**
+   * @inheritdoc
+   */
+  protected function defaultTitle()
+  {
+		return 'AdfsOAuth2';
+  }
+}
--- README.md
+++ README.md
@@ -1,1 +1,39 @@
 # yii2-authclient-adfs
+Adfs client for yii2-authclient
+
+## Installation
+Through Composer
+From console:
+```
+composer require brainsrage/yii2-authclient-instagram
+```
+or add to "require" section to composer.json
+```
+"brainsrage/yii2-authclient-instagram": "dev-master"
+```
+##Usage
+Set up Oauth2 single sign-on on Active Directory with ADFS.
+And add the AdfsOauth2 client to your auth clients.
+
+```
+php
+'components' => [
+    'authClientCollection' => [
+      'class'   => \yii\authclient\Collection::className(),
+      'clients' => [
+        'adfs' => [
+          'class'             => 'macfly\authclient\AdfsOAuth2',
+          'authUrl'           => 'https://adfs.microsoft.com/adfs/oauth2/authorize',
+          'tokenUrl'          => 'https://adfs.microsoft.com/adfs/oauth2/token',
+          'clientId'          => 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx',
+          'clientSecret'      => 'yyyyyyyyy',
+          'userAttributes'    => [
+            'email',
+          ],
+        ],
+        // other clients
+      ],
+    ],
+    // ...
+ ]
+```
